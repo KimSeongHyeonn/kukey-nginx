dddcb564acd61fe603f35b0805377ff844936eb5
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-04-18 16:25:10 +0900

    initial commit
    - 기본 구조 및 배포 script 정의

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "[+] 복사 중: nginx.conf"
+sudo cp ./templates/nginx.conf.template /etc/nginx/nginx.conf
+
+echo "[+] 복사 중: conf.d/*.conf"
+sudo cp ./conf.d/*.conf /etc/nginx/conf.d/
+
+echo "[+] nginx 설정 테스트"
+sudo nginx -t
+
+echo "[+] nginx 재시작"
+sudo systemctl reload nginx
+
+echo "[+] 완료!"
